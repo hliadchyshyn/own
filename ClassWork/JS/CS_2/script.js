9fd7932f28f364ff8cdf8aba913039928b61d3eb
hliadchyshyn <9763795+hliadchyshyn@users.noreply.github.com>
2019-02-09 18:23:08 +0200

    Classwork

---
+++ ClassWork/JS/CS_2/script.js
@@ -0,0 +1,36 @@
+/*
+let firstNumber = Number(prompt("Enter first number", ""));
+let secondNumber = Number(prompt("Enter second number", ""));
+
+if (isNaN(firstNumber) || isNaN(secondNumber)) {
+    alert("YankeeS go home!")
+} else {
+    let operationName = prompt("Enter operation name", "").toLowerCase();
+
+    switch (operationName) {
+        case "+":
+        case "plus":
+            alert(`${firstNumber + secondNumber}`);
+            break;
+
+        case "-":
+        case "minus":
+
+            alert(`${firstNumber - secondNumber}`);
+            break;
+
+        case "*":
+        case "multiply":
+            alert(`${firstNumber * secondNumber}`);
+            break;
+
+        case "/":
+        case "divide":
+            alert(`${firstNumber / secondNumber}`);
+            break;
+
+        default:
+            alert(`Incorrect value`)
+    }
+}*/
+
