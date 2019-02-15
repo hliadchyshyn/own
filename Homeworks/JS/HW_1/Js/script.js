b674bfc710df4732f3feff27dec7be1954967362
I.Hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-02-15 13:29:43 +0200

    Ready for review

---
+++ Homeworks/JS/HW_1/Js/script.js
@@ -0,0 +1,20 @@
+let name = prompt("Enter your name","");
+while (name === "" || name === null){
+    name = prompt('Enter correct name', (name === null ? "" : name));
+}
+let age = prompt("Enter your age","");
+while (isNaN(Number (age))|| age === undefined || age === null){
+    age = Number(prompt('Enter correct age', age));
+}
+if(age < 18){
+    alert('You are not allowed to visit this website');
+} else if(18 <= age <= 22){
+    if (confirm('Are you sure you want to continue?')){
+        alert('Welcome,' +name);
+    } else {
+        alert('You are not allowed to visit this website');
+    }
+} else {
+    alert('Welcome,' +name);
+}
+
