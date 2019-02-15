0c017476320b3a535b5f79a251521f9c98c01b9e
I.Hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-02-15 16:45:55 +0200

    Ready for review

---
+++ Homeworks/JS/HW_2/Js/script.js
@@ -0,0 +1,35 @@
+function checkSimpleNumber(num){
+    while (isNaN(Number (num))|| num === null || num < 1){
+        num = prompt('Enter correct start check number', num);
+    }
+    return Number(num);
+}
+
+function compareNums (num1, num2){
+    if (num1 >= num2){
+        return {bigger:num1, smaller:num2};
+    } else {
+        return {bigger:num2, smaller:num1}
+    }
+}
+function isNatural(num) {
+    if(num < 2) {
+        return false
+    }
+    for (let i = 2; i <= num/2; i++) {
+        if(num%i===0){
+            return false;
+        }
+    }
+    return true;
+}
+
+let startNumber = checkSimpleNumber (prompt('Enter your start check number',''));
+let endNumber = checkSimpleNumber (prompt('Enter your end check number',''));
+
+let i = (compareNums(startNumber, endNumber)).smaller;
+let k = (compareNums(startNumber, endNumber)).bigger;
+
+for(i; i < k; i++){
+    if(isNatural(i)) console.log(i);
+}
