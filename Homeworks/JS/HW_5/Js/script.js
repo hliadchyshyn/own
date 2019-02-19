6ddf3fd86220a646e895b4aa7e288eca092fd5b3
I.Hliadchyshyn <i.s.hliadchyshyn@gmail.com>
2019-02-19 15:23:03 +0200

    Rework to cycle

--- Homeworks/JS/HW_5/Js/script.js
+++ Homeworks/JS/HW_5/Js/script.js
@@ -2,8 +2,22 @@ let someObject = {
     name: 'John',
     age: 30,
     rights: {isCanRead: true, isCanWrite: true, isCanDelete: true},
+    currentDate: String(new Date()),
 };
 
-let clone = Object.assign({}, someObject);
-
+// let clone = Object.assign({}, someObject);
+function cloneObject(objectName) {
+    let clonedName ={};
+    for (let key in objectName){
+        // if (objectName.hasOwnProperty(key)) {
+            if (typeof key === 'object') {
+                cloneObject(key)
+            } else {
+                clonedName[key] = objectName[key];
+            }
+        // }
+    }
+    return clonedName;
+}
+let clone = cloneObject(someObject);
 console.log(clone);
